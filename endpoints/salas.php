63da3d974615403cfd936cbe6a3c727eb54e0d9d
CawanCarv <cawancarvalho3@gmail.com>
2023-06-06 04:03:57 -0300

    musicalweek api v0.1

--- endpoints/salas.php
+++ endpoints/salas.php
@@ -1,12 +1,14 @@
 <?php 
-    header('Access-Control-Allow-Origin: http://localhost:3000');
+    header('Access-Control-Allow-Origin: https://musicalweek.azurewebsites.net');
     header('Access-Control-Allow-Methods: POST');
     header('Access-Control-Allow-Headers: Content-Type, Authorization');
     header('Access-Control-Allow-Credentials: true');
     header('Content-Type: application/json; charset=utf-8');
 
     date_default_timezone_set('america/sao_paulo');
-
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {    
+        return 0;    
+    }
     if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
         http_response_code(405);
         echo json_encode(array('POST' => false));
