9b4600296e8187c1052c576f53cee14c826e1eb2
CawanCarv <cawancarvalho3@gmail.com>
2023-06-20 01:28:35 -0300

    alteração pra produção

--- endpoints/salas.php
+++ endpoints/salas.php
@@ -1,5 +1,5 @@
 <?php 
-    header('Access-Control-Allow-Origin: http://localhost:3000');
+    header('Access-Control-Allow-Origin: https://musicalweek.azurewebsites.net');
     header('Access-Control-Allow-Methods: POST');
     header('Access-Control-Allow-Headers: Content-Type, Authorization');
     header('Access-Control-Allow-Credentials: true');
