4cb0b95d36adb98d937d2b5418adda137c0f72a1
Davi Cavalcanti <davicavalcantirego@gmail.com>
2023-08-17 15:46:36 -0300

    id removido login

--- endpoints/login.php
+++ endpoints/login.php
@@ -42,7 +42,7 @@
 
     try {
         if($usuario->login($conn)){
-            echo json_encode(array('id_usuario' => $usuario->getid($conn)));
+            // echo json_encode(array('id_usuario' => $usuario->getid($conn)));
             http_response_code(200);
             echo json_encode(array('token' => gerarToken($usuario->getid($conn))));
         } else {
