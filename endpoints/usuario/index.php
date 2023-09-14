a5f6bb986a2780594e1a8131b513dabead215649
Davi Cavalcanti <davicavalcantirego@gmail.com>
2023-09-13 22:10:36 -0300

    teste

--- endpoints/usuario/index.php
+++ endpoints/usuario/index.php
@@ -9,10 +9,10 @@ header('Content-Type: application/json; charset=utf-8');
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {    
     return 0;    
 }
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     include("../../token/auth/auth.php");
     include("perfil.php");
-} elseif ($_SERVER['REQUEST_METHOD'] === 'PUT') {
+} elseif ($_SERVER['REQUEST_METHOD'] == 'PUT') {
     include("../../token/auth/auth.php");
     $vars = json_decode(file_get_contents('php://input'), true);
     if (isset($vars['plano'])) {
@@ -22,14 +22,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     } else {
         include("altera.php");
     }
-} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+} elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $vars = json_decode(file_get_contents('php://input'), true);
     if (!isset($vars['nome']) && !isset($vars['nick']) && !isset($vars['data_nasc'])) {
         include("login.php");
     } else {
         include("cadastro.php");
     }
-} elseif ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+} elseif ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
     include("../../token/auth/auth.php");
     include("deleta.php");
 } else {
