c80eb8d7d29a26fb9f2f8a3ea82f77a51f13a124
Davi Cavalcanti <davicavalcantirego@gmail.com>
2023-09-17 17:25:11 -0300

    https to http dnv

--- endpoints/usuario/index.php
+++ endpoints/usuario/index.php
@@ -1,6 +1,6 @@
 <?php
 
-header('Access-Control-Allow-Origin: https://localhost:3000');
+header('Access-Control-Allow-Origin: http://localhost:3000');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Methods: POST, GET, PUT, DELETE');
