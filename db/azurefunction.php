5f7d976f538b90019bd1231fd081ea12176fc936
Davi Cavalcanti <davicavalcantirego@gmail.com>
2023-10-30 20:45:04 -0300

    Azure Function

---
+++ db/azurefunction.php
@@ -0,0 +1,28 @@
+<?php
+    header('Access-Control-Allow-Methods: GET');
+    header('Content-Type: application/json; charset=utf-8');
+
+    if ($_GET['codigo'] != "26Kf7A0m2X9rl5Ai2") {
+        http_response_code(500);
+        exit;
+    }
+
+    require_once("dbconexao.php");
+    
+    try {
+        $stmt = $conn->prepare("INSERT_AZURE_FUNCTION");
+        $stmt->execute();
+        http_response_code(200);
+        echo json_encode(array(
+            "sucesso" => true,
+        ), JSON_UNESCAPED_UNICODE);
+        exit;
+    }
+    catch (PDOException $e) {
+        http_response_code(500);
+        echo json_encode(array(
+            "erro" => $ex->getMessage(),
+        ), JSON_UNESCAPED_UNICODE);
+        exit;
+    }
+?>
